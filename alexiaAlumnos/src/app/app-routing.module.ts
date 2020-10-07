aee46be76de265924cc7698cee52017ce32e2c86
julio <juliorod63@gmail.com>
2020-10-07 17:18:42 -0300

    todos los tabs

--- alexiaAlumnos/src/app/app-routing.module.ts
+++ alexiaAlumnos/src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./pages/tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./pages/tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 ];
 @NgModule({
